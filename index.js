ed42d4f4e60dc7084b8059720af4c77fdf81368f
Joseph Kisbye <josephkisbye@gmail.com>
2024-08-29 11:59:30 -0400

    break out functions, introduce type weakness check

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
-import axios, { isCancel } from "axios";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
-const url = "https://pokeapi.co/api/v2/pokemon/";
+const endpointName = "https://pokeapi.co/api/v2/pokemon/";
+const endpointType = "https://pokeapi.co/api/v2/type/"
 const typeIcons = {
   bug : "assets/bug-type.png",
   dark : "assets/dark-type.png",
@@ -25,9 +26,43 @@ const typeIcons = {
   water : "assets/water-type.png",
 }
 
+async function checkType(u, n) {
+  const response = await axios.get(u + n);
+  const result = response.data;
+  const pokeSprite = result.sprites.front_default;
+  const typeOne = result.types[0].type.name;
+  const typeOneIcon = typeIcons[typeOne]
+  if (result.types[1]) {
+    var typeTwo = result.types[1].type.name;
+    var typeTwoIcon = typeIcons[typeTwo];
+  }
+  else { 
+    var typeTwo = "";
+    var typeTwoIcon = "";
+   };
+return { 
+  pName : n,
+  pSprite : pokeSprite,
+  pTypes : { 
+    pTypeOne : typeOne,
+    pTypeTwo : typeTwo,
+    pTypeOneIcon : typeOneIcon,
+    pTypeTwoIcon : typeTwoIcon}
+}};
+
+async function checkTypeWeaknesses(u, t) {
+  const response = await axios.get(u + t);
+  const result = response.data;
+  const noDamangeFrom = result.damage_relations.no_damage_from[0].name;
+  const halfDamageFrom = result.damage_relations.half_damage_from;
+  const doubleDamageFrom = result.damage_relations.double_damage_from;
+  console.log(noDamangeFrom)
+  console.log(halfDamageFrom)
+  console.log(doubleDamageFrom)
+}
+
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-
 
 app.get("/", (req, res) => {    
     res.render("index.ejs");
@@ -36,28 +71,9 @@ app.get("/", (req, res) => {
 app.post("/", async (req, res) => {
     const searchTerm = req.body.pokemonName.toLowerCase();
     try {
-    const response = await axios.get(url + searchTerm);
-    const result = response.data;
-    const pokeSprite = result.sprites.front_default;
-    const typeOne = result.types[0].type.name;
-    const typeOneIcon = typeIcons[typeOne]
-    if (result.types[1]) {
-      var typeTwo = result.types[1].type.name;
-      var typeTwoIcon = typeIcons[typeTwo];
-    }
-    else { 
-      var typeTwo = "";
-      var typeTwoIcon = "";
-     };
-    res.render("index.ejs", { data : { 
-      pName : searchTerm,
-      pSprite : pokeSprite,
-      pTypes : { 
-        pTypeOne : typeOne,
-        pTypeTwo : typeTwo,
-        pTypeOneIcon : typeOneIcon,
-        pTypeTwoIcon : typeTwoIcon}
-    }});
+    var pokeData = await checkType(endpointName, searchTerm);
+    await checkTypeWeaknesses(endpointType, pokeData.pTypes.pTypeOne)
+    res.render("index.ejs", { data : pokeData});
     } catch (error) {
       console.error("Failed to make request:", error.message);
       res.render("index.ejs", {
