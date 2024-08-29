0deca3645b7e34c3de42d4366dd0661e607db51c
Joseph Kisbye <josephkisbye@gmail.com>
2024-08-29 14:49:47 -0400

    add basic logic for matchup vulnerabilities

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 const endpointName = "https://pokeapi.co/api/v2/pokemon/";
-const endpointType = "https://pokeapi.co/api/v2/type/"
+const endpointType = "https://pokeapi.co/api/v2/type/";
+
 const typeIcons = {
   bug : "assets/bug-type.png",
   dark : "assets/dark-type.png",
@@ -24,6 +25,27 @@ const typeIcons = {
   rock : "assets/rock-type.png",
   steel : "assets/steel-type.png",
   water : "assets/water-type.png",
+}
+
+const typeVulnerabilities = {
+  bug : 1,
+  dark : 1,
+  dragon : 1,
+  electric : 1,
+  fairy : 1,
+  fighting : 1,
+  fire : 1,
+  flying : 1,
+  ghost : 1,
+  grass : 1,
+  ground : 1,
+  ice : 1,
+  normal : 1,
+  poison : 1,
+  psychic : 1,
+  rock : 1,
+  steel : 1,
+  water : 1,
 }
 
 async function checkType(u, n) {
@@ -51,15 +73,28 @@ return {
 }};
 
 async function checkTypeWeaknesses(u, t) {
+  var dbl = [];
+  var hlf = [];
+  var imn = [];
+
   const response = await axios.get(u + t);
   const result = response.data;
-  const noDamangeFrom = result.damage_relations.no_damage_from[0].name;
-  const halfDamageFrom = result.damage_relations.half_damage_from;
-  const doubleDamageFrom = result.damage_relations.double_damage_from;
-  console.log(noDamangeFrom)
-  console.log(halfDamageFrom)
-  console.log(doubleDamageFrom)
-}
+
+  result.damage_relations.double_damage_from.forEach(type => {
+    dbl.push(type.name)
+  })
+  result.damage_relations.half_damage_from.forEach(type => {
+    hlf.push(type.name)
+  })
+  result.damage_relations.no_damage_from.forEach(type => {
+    imn.push(type.name)
+  })
+
+  return {
+    double: dbl, 
+    half: hlf,
+    immune: imn};
+};
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -72,7 +107,14 @@ app.post("/", async (req, res) => {
     const searchTerm = req.body.pokemonName.toLowerCase();
     try {
     var pokeData = await checkType(endpointName, searchTerm);
-    await checkTypeWeaknesses(endpointType, pokeData.pTypes.pTypeOne)
+    var pokeWeaknessA = await checkTypeWeaknesses(endpointType, pokeData.pTypes.pTypeOne);
+    if (pokeData.pTypes.pTypeTwo !== "") {
+      var pokeWeaknessB = await checkTypeWeaknesses(endpointType, pokeData.pTypes.pTypeTwo)
+    }
+    console.log(`${pokeData.pTypes.pTypeOne} is weak to`)
+    console.log(pokeWeaknessA);
+    console.log(`${pokeData.pTypes.pTypeTwo} is weak to`)
+    console.log(pokeWeaknessB);
     res.render("index.ejs", { data : pokeData});
     } catch (error) {
       console.error("Failed to make request:", error.message);
